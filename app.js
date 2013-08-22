86d9c962512a5be5db86e8ad8ee039f6dde3b6df
Prekolna <Prekolna@gmail.com>
2013-08-22 03:21:56 +0000

    Added stylesheet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
-  , controllers = require('./controllers');
+  , controllers = require('./controllers')
+  , lessMiddleware = require('less-middleware');
   
 var app = express();
 
@@ -7,7 +8,12 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set(lessMiddleware({
+    src: __dirname + '/public/css',
+    compress: true 
+  }));
   app.set(express.favicon(__dirname + 'favicon.ico'));
+  app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
